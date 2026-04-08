09be0fbd3462a776b99edffbb27578cb72e0b6dc
suhas1981 <suhas.s2004manu@gmail.com>
2026-04-08 15:07:54 +0530

    Create add.py
    second commit

---
+++ add.py
@@ -0,0 +1,9 @@
+# add_numbers.py
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = 5
+    num2 = 7
+    result = add_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is {result}")
